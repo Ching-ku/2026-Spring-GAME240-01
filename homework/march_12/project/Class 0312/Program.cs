79e48f1762fef1e84420475f02809e92be252109
Ching-ku <chku@chapman.edu>
2026-03-12 20:05:17 -0700

    hw 0312

---
+++ homework/march_12/project/Class 0312/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] animals = {"panda","dog", "penguin", "giraffe"};
+string favoriteAnimal = "giraffe";
+
+int i;
+i = 0;
+
+bool x;
+x = false;
+
+foreach (string animal in animals)
+{
+    
+    if (animal == favoriteAnimal)
+    {
+        x = true;
+        break;
+    }
+}
+
+if (x == true)
+{
+    Console.WriteLine("Yay");
+}
+
+if (x == false)
+{
+    Console.WriteLine("Nay");
+}
---
+++ homework/march_12/project/HW 0312/Program.cs
@@ -0,0 +1,23 @@
+﻿int [] numbers = {1,2,8,103,10,268};
+int smallest = numbers[0];
+int biggest = numbers[0];
+
+int index = 0;
+
+while (index < numbers.Length)
+{
+    if (numbers[index] < smallest)
+    {
+        smallest = numbers[index];
+    }
+
+    if (numbers[index] > biggest)
+    {
+        biggest = numbers[index];
+    }
+
+    index++;
+}
+
+Console.WriteLine("The largest number is " + biggest);
+Console.WriteLine("The smallest number is " + smallest);
